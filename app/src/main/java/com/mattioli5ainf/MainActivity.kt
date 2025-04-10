9b0824661de2168a1a83dad8ba4fb4517fcbaf9b
Filippo Mattioli <filippomattioli06@gmail.com>
2025-04-10 10:24:26 +0200

    Creazione progetto

---
+++ app/src/main/java/com/mattioli5ainf/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.mattioli5ainf
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        var btnRoll : Button = findViewById(R.id.buttonRoll)
+        btnRoll.setOnClickListener(View.OnClickListener {
+            var toast = Toast.makeText(this, "LANCIO DEL DADO", Toast.LENGTH_LONG)
+            toast.show()
+
+            var mioIntent : Intent = Intent(this, SecondActivity::class.java)
+            var mioRandom = (1..6).random()
+            mioIntent.putExtra("MESSAGE", "Numero estratto: $mioRandom")
+            startActivity(mioIntent)
+
+        })
+    }
+}
